b3a2239743e04fbfef6af09b522f9941f6c94c77
Chanatip Rawime <tipnacha510@gmail.com>
2025-03-04 12:45:45 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[]) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[ 50 ] ;
+    char *output ;
+    printf( "Enter str1 : " );
+    fgets( text, sizeof(text), stdin ) ;
+
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
+}
+
+char str2[ 50 ] ;
+char* reverse( char str1[]) {
+    int length ;
+    length = strlen( str1 ) ;
+
+    for ( int i = 0 ; i < length ; i++ ) {
+        str2[i] = str1[ length - 1 - i ] ;
+    } 
+    str2[ length ] = '\0' ;
+    printf( "FUNC : %s\n",str2) ;
+    return str2 ;
+}
